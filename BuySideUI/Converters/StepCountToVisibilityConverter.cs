bb1222bc4ceeb09b5bc6c1d8c206bd77b690a33e
RossB <Ross@home>
2017-02-05 23:10:35 -0500

    Show gateway only when buy side step count > 0

---
+++ BuySideUI/Converters/StepCountToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BuySideUI
+{
+	public class StepCountToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var count = (int) value;
+			return count > 0 ? Visibility.Visible : Visibility.Hidden;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
